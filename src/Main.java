29f3da32a9bf62bfedcdbcf824ca39cf751c1ff0
Salino3 <flaviobcn012@gmail.com>
2024-09-15 11:13:37 +0200

    execute a method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        Sum obj1 = new Sum();
+        // print and jump of line
+        System.out.println(obj1.age);
+
         System.out.println("Hello, World!");
+
+        // * Static method is called directly from the class and not from the instance
+        // obj1.sumNumbers(args);
+        Sum.sumNumbers(args);
     }
 }
---
+++ src/Sum.java
@@ -0,0 +1,13 @@
+public class Sum {
+    int age = 25;
+
+    public static void sumNumbers(String args[]) {
+        int num_1 = 5;
+        int num_2 = 2;
+        int result;
+
+        result = num_1 + num_2;
+        System.out.println(
+                "The sum of " + num_1 + " and " + num_2 + " is: " + result);
+    }
+}
